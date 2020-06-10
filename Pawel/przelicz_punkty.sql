992d4723c6dd9b36d8346fc01c52fe477436993e
pawezim <62487399+pawezim@users.noreply.github.com>
2020-06-10 21:47:28 +0200

    Update przelicz_punkty.sql

--- Pawel/przelicz_punkty.sql
+++ Pawel/przelicz_punkty.sql
@@ -1,12 +1,24 @@
 CREATE OR REPLACE PROCEDURE przelicz_punkty
-(turniej_id integer, system_punktacji in char(3))
+(turniej_id INTEGER, system_punktacji VARCHAR)
 AS
-
-
 
 BEGIN
 
+IF system_punktacji='sum' THEN
+  UPDATE rozgrywki SET punkty_truniejowe_dla_ns=punkty_dla_ns
+    FROM rozgrywki JOIN rozdania USING rozdanie_id
+    WHERE rozdania.turniej_id=turniej_id;
+  
+  UPDATE pary SET pary.suma_punktow_turniejowych=sum(rozgrywki.punkty_turniejowe_dla_ns)
+    FROM rozgrywki JOIN rozdania USING rozdanie_id
+    WHERE rozdania.turniej_id=turniej_id
+    AND rozgrywki.para_ns_id=pary.para_id;
+
+  UPDATE pary SET pary.suma_punktow_turniejowych=(-1)*sum(rozgrywki.punkty_turniejowe_dla_ns)
+    FROM rozgrywki JOIN rozdania USING rozdanie_id
+    WHERE rozdania.turniej_id=turniej_id
+    AND rozgrywki.para_we_id=pary.para_id;
+
+END IF;
 
 END;
-
-/
