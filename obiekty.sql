161202528aeb47f4d625905b18ca9bcfadfc4ae1
pawezim <62487399+pawezim@users.noreply.github.com>
2020-06-10 18:35:51 +0200

    Update obiekty.sql

--- obiekty.sql
+++ obiekty.sql
@@ -156,11 +156,11 @@ gracz_w integer;
 
 BEGIN
 
-select gracz_1 into gracz_n from pary where para_id=id_pary_ns;
-select gracz_2 into gracz_s from pary where para_id=id_pary_ns;
+select gracz_1_id into gracz_n from pary where para_id=id_pary_ns;
+select gracz_2_id into gracz_s from pary where para_id=id_pary_ns;
 
-select gracz_1 into gracz_w from pary where para_id=id_pary_we;
-select gracz_2 into gracz_e from pary where para_id=id_pary_we;
+select gracz_1_id into gracz_w from pary where para_id=id_pary_we;
+select gracz_2_id into gracz_e from pary where para_id=id_pary_we;
 
 insert into rozgrywki values(
 turnieje_seq.nextval,
