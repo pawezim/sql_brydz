3c0eda40c2b4eb62d04b64fb48fd81cfbeaf5a5d
pawezim <62487399+pawezim@users.noreply.github.com>
2020-06-10 19:52:31 +0200

    Update sekwencje.sql

--- sekwencje.sql
+++ sekwencje.sql
@@ -9,6 +9,6 @@ create sequence rozdania_seq start with 1;
 create sequence gracze_seq start with 1;
 create sequence pary_seq start with 1;
 create sequence turnieje_seq start with 1;
-create sequence rozgrywki_seq start with 2;
+create sequence rozgrywki_seq start with 1;
 create sequence lewy_seq start with 1;
 
