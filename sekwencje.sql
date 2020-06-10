ce4ff6e2473a0c67f410905d88a091a4b470e67c
pawezim <62487399+pawezim@users.noreply.github.com>
2020-06-10 17:28:01 +0200

    Add files via upload

---
+++ sekwencje.sql
@@ -0,0 +1,14 @@
+drop sequence rozdania_seq;
+drop sequence gracze_seq;
+drop sequence pary_seq;
+drop sequence turnieje_seq;
+drop sequence rozgrywki_seq;
+drop sequence lewy_seq;
+
+create sequence rozdania_seq start with 1;
+create sequence gracze_seq start with 1;
+create sequence pary_seq start with 1;
+create sequence turnieje_seq start with 1;
+create sequence rozgrywki_seq start with 2;
+create sequence lewy_seq start with 1;
+
